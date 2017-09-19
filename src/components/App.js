17a58d151651f2a561689e20baaa73a7406e86bf
Aaron Anderson <aaronlynanderson@gmail.com>
2017-09-19 10:59:05 -0400

    Placeholder for Footerrenders from App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
 import '../styles/App.css';
+import Footer from './footer.js';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
 
+        <Footer />
       </div>
     );
   }
---
+++ src/components/footer.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import '../styles/App.css';
+
+class Footer extends Component {
+  render() {
+    return (
+      <div className="Footer">
+        <p>Footer Placeholder</p>
+      </div>
+    );
+  }
+}
+
+export default Footer;
