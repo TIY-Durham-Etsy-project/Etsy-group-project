8140c277655adc6aa1c118c3b26ee6a757b13cca
Aaron Anderson <aaronlynanderson@gmail.com>
2017-09-19 18:55:37 -0400

    SellLinks component renders within FooterLinks component

--- src/components/footer-links/sell-links.js
+++ src/components/footer-links/sell-links.js
@@ -4,6 +4,18 @@ class SellLinks extends Component {
   render() {
     return (
       <div className="SellLinks">
+        <h3>Sell</h3>
+        <ul>
+          <li>
+            <a href="https://www.etsy.com/sell?ref=ftr">Sell on Etsy</a>
+          </li>
+          <li>
+            <a href="https://www.etsy.com/teams?ref=ftr">Teams</a>
+          </li>
+          <li>
+            <a href="https://www.etsy.com/forums?ref=ftr">Forums</a>
+          </li>
+        </ul>
       </div>
     );
   }
