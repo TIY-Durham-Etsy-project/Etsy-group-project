73ee24e70cd2fdce3ec7cba0e25e0afd95c9e385
Aaron Anderson <aaronlynanderson@gmail.com>
2017-09-19 14:53:41 -0400

    HelpAndCurrency placeholder renders in footer

--- src/components/footer.js
+++ src/components/footer.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import '../styles/App.css';
 import OptInForm from './opt-in-form.js'
 import Trademark from './trademark.js'
+import HelpAndCurrency from './help-and-currency.js'
 
 class Footer extends Component {
   render() {
@@ -9,6 +10,7 @@ class Footer extends Component {
       <div className="Footer">
         <p>Footer Placeholder</p>
         <OptInForm />
+        <HelpAndCurrency />
         <Trademark />
       </div>
     );
---
+++ src/components/help-and-currency.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import '../styles/App.css';
+
+
+class HelpAndCurrency extends Component {
+  render() {
+    return (
+      <div className="HelpAndCurrency">
+        <p>Help and Currency Placeholder</p>
+      </div>
+    );
+  }
+}
+
+export default HelpAndCurrency;
