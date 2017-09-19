cc5808f30af98a365cc3422b8a60920f1578300d
Kevin Harrison <kharrison7@elon.edu>
2017-09-19 16:42:47 -0400

    This makes the big carousel image appear as the first image given.

--- src/components/imagecarousel.js
+++ src/components/imagecarousel.js
@@ -4,8 +4,10 @@ export default class ImageCarousel extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      pictures: []
+      mainPicture: false,
+      imageArray: {}
     }
+    this.handlePictureChange = this.handlePictureChange.bind(this);
   }
 
   componentDidMount() {
@@ -27,38 +29,50 @@ export default class ImageCarousel extends Component {
     // }
   }
 
+  handlePictureChange(event){
+    this.setState({
+        mainPicture: event.target.value
+      })
+  }
+
   render(){
     let imageItem = false;
-    let link = {};
+    // let link = {};
     if(this.props.imagesdata[0] !== undefined){
+      // this.setState({
+      //     mainPicture: this.props.imagesdata[0].url_75x75
+      // });
+      this.state.mainPicture = this.props.imagesdata[0].url_570xN;
       console.log(this.props.imagesdata);
-      let imagesdata = this.props.imagesdata;
-      for(let i = 0; i < imagesdata.length; i++ ){
-        link[i] = this.props.imagesdata[i].url_75x75;
-      }
+      // let imagesdata = this.props.imagesdata;
+      // for(let i = 0; i < imagesdata.length; i++ ){
+      //   link[i] = this.props.imagesdata[i].url_75x75;
+      // }
 
     let count = 0;
     imageItem = this.props.imagesdata.map(image => {
       console.log(image);
+      this.state.imageArray[{count}] = image.url_75x75;
       count++;
       return (
         <div key = {image.id} className = "itemsBoxes">
-          <div className="card" id={count}
-            // onClick={this.handleImageClick}
-            >
+          <div className="card" id={count} onClick={this.handleSongChange}>
             <div className="card-block">
               <img src={image.url_75x75} />
             </div>
           </div>
         </div>
         )
+        // console.log(this.state.imageArray);
       })
+      // console.log(this.state.imageArray);
       }
     return(
       <div className="imagecarousel">
         <p>Carousel:</p>
         {/* <img src={link[0]} />
         <img src={link[1]} /> */}
+        <img src={this.state.mainPicture} />
         {imageItem}
         {/* {this.state.imageItems} */}
          {/* {this.state.pictures} */}
