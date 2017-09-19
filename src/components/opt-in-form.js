93bd8474a6227815eb8d01aff35e0ee1540081af
Aaron Anderson <aaronlynanderson@gmail.com>
2017-09-19 11:04:18 -0400

    Opt-in-form placeholder renders within footer container

---
+++ src/components/opt-in-form.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import '../styles/App.css';
+
+
+class Opt_in_form extends Component {
+  render() {
+    return (
+      <div className="Opt_in_form">
+        <p>Opt in form placeholder</p>
+      </div>
+    );
+  }
+}
+
+export default Opt_in_form;
