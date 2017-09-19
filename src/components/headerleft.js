6ae936e62d12059b34991b967d5b8fb901b480fd
Hannah Hudson <hannah.hch@gmail.com>
2017-09-18 20:32:51 -0400

    added styles for the header left (and right)

--- src/components/headerleft.js
+++ src/components/headerleft.js
@@ -4,6 +4,22 @@ export default class Headerleft extends Component {
   render(){
     return(
       <div className="headerleft">
+        <div className ='header-left-items'>
+          <div className = "shop-icon">
+            <img src = "#" alt = "shop icon"/>
+          </div>
+          <div className = "shop-name fav-btn">
+            <h2>Shop Name Here</h2>
+            <button className = "fav-heart">&hearts; Favorite Shop</button>
+          </div>
+        </div>
+        <div className = "header-right-items">
+          <div className = "sample-item"><img src = "" alt = "store item img"/></div>
+          <div className = "sample-item"><img src = "" alt = "store item img"/></div>
+          <div className = "sample-item"><img src = "" alt = "store item img"/></div>
+          <div className = "sample-item"><img src = "" alt = "store item img"/></div>
+          <div className = "sample-item sample-item-count">number of items</div>
+        </div>
       </div>
     )
   }
