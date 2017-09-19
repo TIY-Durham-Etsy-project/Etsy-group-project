ff5e23952a94752aa9c2f46b1ec897095ecb537f
Aaron Anderson <aaronlynanderson@gmail.com>
2017-09-19 19:36:19 -0400

    ShareLinks component markup is ready to receive hyperlinks and svg data

---
+++ src/components/share-links.js
@@ -0,0 +1,51 @@
+import React, { Component } from 'react';
+
+class ShareLinks extends Component {
+  render() {
+    return (
+      <div className="ShareLinks">
+
+        <ul>
+          <li>
+            <a href="https://www.etsy.com/giftcards?ref=ftr">
+              <span>
+                <svg>
+                  <path />
+                </svg>
+              </span>
+            </a>
+          </li>
+          <li>
+            <a href="https://blog.etsy.com/en/?ref=ftr">
+              <span>
+                <svg>
+                  <path />
+                </svg>
+              </span>
+            </a>
+          </li>
+          <li>
+            <a href="https://www.etsy.com/wholesale?ref=ftr">
+              <span>
+                <svg>
+                  <path />
+                </svg>
+              </span>
+            </a>
+          </li>
+          <li>
+            <a href="https://www.etsy.com/wholesale?ref=ftr">
+              <span>
+                <svg>
+                  <path />
+                </svg>
+              </span>
+            </a>
+          </li>
+        </ul>
+      </div>
+    );
+  }
+}
+
+export default ShareLinks;
