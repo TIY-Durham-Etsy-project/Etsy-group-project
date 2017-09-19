77f48782726dba142dbd6a440a551c48f925c43e
Aaron Anderson <aaronlynanderson@gmail.com>
2017-09-19 16:21:59 -0400

    HelpAndCurrency is built out, correct img sources need to be swapped out with api call to currency application

--- src/components/help-and-currency.js
+++ src/components/help-and-currency.js
@@ -7,6 +7,28 @@ class HelpAndCurrency extends Component {
     return (
       <div className="HelpAndCurrency">
         <p>Help and Currency Placeholder</p>
+        <div className="float-left">
+          <img src={require("./logo.svg")} alt="?" height='20'/>
+          <span>
+            Need help? Visit the
+            <a href="https://www.etsy.com/help?ref=ftr">help center</a>
+          </span>
+        </div>
+        {this.props.children}
+        <div className="float-right">
+          <div className="display-table">
+            <div className="display-table-cell">
+              <img src={require("./logo.svg")} alt="flag" height='20'/>
+              United States
+            </div>
+            <div className="display-table-cell">
+              English (USD)
+            </div>
+            <div className="display-table-cell">
+              $(USD)
+            </div>
+          </div>
+        </div>
       </div>
     );
   }
--- src/components/trademark.js
+++ src/components/trademark.js
@@ -9,7 +9,7 @@ class Trademark extends Component {
         <div className="float-left">
           <div className="display-table">
             <div className="display-table-cell">
-              <img src={require('./logo.svg')} alt="Etsy Logo"/>
+              <img src={require('./logo.svg')} alt="Etsy Logo" height="20"/>
             </div>
             <div className="display-table-cell">
               "We make it easier to find your thing."
