cd07345d24d0b3b222e541c0fb54fc94a747c29c
Aaron Anderson <aaronlynanderson@gmail.com>
2017-09-19 19:27:48 -0400

    AboutLinks component renders in FooterLinks component

--- src/components/footer-links/about-links.js
+++ src/components/footer-links/about-links.js
@@ -5,7 +5,25 @@ class AboutLinks extends Component {
   render() {
     return (
       <div className="AboutLinks">
-      </div>
+        <h3>About</h3>
+        <ul>
+          <li>
+            <a href="https://www.etsy.com/about?ref=ftr">Etsy, Inc.</a>
+          </li>
+          <li>
+            <a href="https://www.etsy.com/legal?ref=ftr">Policies</a>
+          </li>
+          <li>
+            <a href="https://investors.etsy.com/">Investors</a>
+          </li>
+          <li>
+            <a href="https://www.etsy.com/careers?ref=ftr">Careers</a>
+          </li>
+          <li>
+            <a href="https://www.etsy.com/press?ref=ftr">Press</a>
+          </li>
+        </ul>
+        </div>
     );
   }
 }
