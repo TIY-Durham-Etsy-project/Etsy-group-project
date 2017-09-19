cb9b719ae3adc8ce6931fa7110683ee058a05079
Jonathan Canfield <jonathantcanfield@gmail.com>
2017-09-19 19:00:49 -0400

    add notes

--- src/components/App.js
+++ src/components/App.js
@@ -6,15 +6,16 @@ import ItemPageApp from './itempageApp.js';
 class App extends Component {
   constructor(props) {
     super(props);
-
+    //There should be some kind of "state" that starts empty.
+    //There should be a turnery statement that says "if state is empty, show frontpage. if it has a number, show itempage"
   }
 
   render() {
     return (
       <div className="App">
         <Header/>
+        //FRONT PAGE SHOULD GO HERE
         <ItemPageApp/>
-      
       </div>
     );
   }
