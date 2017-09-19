a872740aace5f4f01c856187885551c8219c8c80
Tivona Spruill <tivona.spruill@yahoo.com>
2017-09-19 12:22:07 -0400

    added social media buttons and styling

--- src/components/socialbuttons.js
+++ src/components/socialbuttons.js
@@ -4,6 +4,15 @@ export default class SocialButtons extends Component {
   render(){
     return(
       <div className="socialbuttons">
+      <div clasName="add-to">
+      <button className = "fav-heart">&hearts; Favorite </button>
+      <button className = "add-to">&#9776; Add to &#9662; </button>
+      <hr/>
+      </div>
+      <a href="http://fontawesome.io/icon/facebook-official/" className="fa fa-facebook">Share</a>
+      <a href="http://fontawesome.io/icon/pinterest/" className="fa fa-pinterest"> Save </a>
+      <a href="http://fontawesome.io/icon/twitter/" className="fa fa-twitter"> Tweet </a>
+      <hr/>
       </div>
     )
   }
