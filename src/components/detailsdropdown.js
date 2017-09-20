983d8412e66d58ef34fd04c136d1a119011314d0
Jonathan Canfield <jonathantcanfield@gmail.com>
2017-09-20 10:53:33 -0400

    Add mount to details page

--- src/components/detailsdropdown.js
+++ src/components/detailsdropdown.js
@@ -1,12 +1,25 @@
 import React, { Component } from 'react';
 
 export default class DetailsDropdown extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      lineheight: 0
+    }
+  }
+  componentWillMount(){
+    let detailsdropdown = document.getElementsByClassName('detailsdropdown')[0];
+    if (detailsdropdown !== undefined){
+      const value = document.defaultView.getComputedStyle(detailsdropdown, null);
+      console.log(value.height)
+    }
+  }
   render(){
-    console.log(this.props);
+    console.log("RENDER");
     return(
       <div className="detailsdropdown">
       <h3>Details</h3>
-      <p>{this.props.listingdata ? this.innerHTML = this.props.listingdata.description : ""}</p>
+      <p>{this.props.listingdata ? this.props.listingdata.description.replace(/&/g, "&amp;").replace(/>/g, "&gt;").replace(/</g, "&lt;").replace(/"/g, "&quot;") : ""}</p>
       </div>
     )
   }
