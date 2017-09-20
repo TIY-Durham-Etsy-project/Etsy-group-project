1a6eabf79146d78b3df732d5fb48535777fc7616
Kevin Harrison <kharrison7@elon.edu>
2017-09-19 20:59:04 -0400

    This adds a button that lets the user scroll through the images.

--- src/components/imagecarousel.js
+++ src/components/imagecarousel.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import '../styles/App.css';
 
 export default class ImageCarousel extends Component {
   constructor(props) {
@@ -9,10 +10,11 @@ export default class ImageCarousel extends Component {
       imageCarouselValue: 0
     }
     this.handlePictureChange = this.handlePictureChange.bind(this);
+    this.minusSlides = this.minusSlides.bind(this);
+    this.plusSlides = this.plusSlides.bind(this);
   }
 
   componentDidMount() {
-
     if(this.props.imagesdata[0] !== undefined){
       this.setState({
           mainPicture: this.state.imageCarouselValue
@@ -28,15 +30,54 @@ export default class ImageCarousel extends Component {
         mainPicture: this.props.imagesdata[event.currentTarget.id-1].url_570xN,
         ImageCarouselValue: this.props.imagesdata[event.currentTarget.id-1].url_570xN
       })
-    this.state.imageCarouselValue = this.props.imagesdata[event.currentTarget.id-1].url_570xN
-
+    this.state.imageCarouselValue = event.currentTarget.id-1;
     this.state.mainPicture = this.props.imagesdata[event.currentTarget.id-1].url_570xN;
     console.log("mainPicture src: ", this.state.mainPicture);
     // this.setState({
     //     mainPicture: this.props.imagesdata[event.currentTarget.id-1].url_570xN
     //   })
-    let play_Song = document.getElementById('music_Here');
-    play_Song.src = this.state.mainPicture;
+    let place_Image = document.getElementById('mainPictureForCarousel');
+    place_Image.src = this.state.mainPicture;
+  }
+
+  // plusSlides(n) {
+  // this.state.imageCarouselValue += n;
+  // this.state.mainPicture = this.props.imagesdata[this.state.imageCarouselValue].url_570xN;
+  // let place_Image = document.getElementById('mainPictureForCarousel');
+  // place_Image.src = this.state.mainPicture;
+  // console.log(this.state.mainPicture);
+  // }
+
+  minusSlides(){
+    this.state.imageCarouselValue = this.state.imageCarouselValue - 1;
+    this.state.mainPicture = this.props.imagesdata[this.state.imageCarouselValue].url_570xN;
+    let place_Image = document.getElementById('mainPictureForCarousel');
+    place_Image.src = this.state.mainPicture;
+    console.log(this.state.mainPicture);
+  }
+  plusSlides(){
+    // this.state.imageCarouselValue = this.state.imageCarouselValue + 1;
+    this.state.imageCarouselValue++;
+    let x = this.state.imageCarouselValue - 1;
+    this.setState({
+        ImageCarouselValue: x + 1
+      })
+    if(x > this.props.imagesdata){
+      this.state.imageCarouselValue = 0;
+    }
+    if(this.props.imagesdata[this.state.imageCarouselValue] === undefined){
+      this.setState({
+          ImageCarouselValue: 0
+        })
+        this.state.imageCarouselValue = 0;
+    }
+    console.log(this.state.imageCarouselValue);
+
+    this.state.mainPicture = this.props.imagesdata[this.state.imageCarouselValue].url_570xN;
+
+    let place_Image = document.getElementById('mainPictureForCarousel');
+    place_Image.src = this.state.mainPicture;
+    console.log(this.state.mainPicture);
   }
 
   render(){
@@ -73,7 +114,15 @@ export default class ImageCarousel extends Component {
         <p>Carousel:</p>
         {/* <img src={link[0]} />
         <img src={link[1]} /> */}
-        <img id="music_Here" src={this.state.mainPicture} />
+        <div class="slideshow-container">
+
+        <img id="mainPictureForCarousel" src={this.state.mainPicture} />
+        {/* <a class="prev" onclick={this.minusSlides}>&#10094;</a>
+        <a class="next" onclick={this.plusSlides}>&#10095;</a> */}
+        <form className="button">
+              <button onClick={this.plusSlides}  id="countDown" type="button" className="btn btn-success">Change Image: {this.state.imageCarouselValue}</button>
+            </form>
+        </div>
         {imageItem}
         {/* {this.state.imageItems} */}
          {/* {this.state.pictures} */}
