9d1de5ffcece2b789d7a7e235ed17cc9781f7c46
Kevin Harrison <kharrison7@elon.edu>
2017-09-19 21:19:34 -0400

    This adds an up and a down button.

--- src/components/imagecarousel.js
+++ src/components/imagecarousel.js
@@ -14,87 +14,69 @@ export default class ImageCarousel extends Component {
     this.plusSlides = this.plusSlides.bind(this);
   }
 
-  componentDidMount() {
-    if(this.props.imagesdata[0] !== undefined){
-      this.setState({
-          mainPicture: this.state.imageCarouselValue
-      })
-    }
-  }
-
+  // This brings up a sub-image to the main image position when clicked.
   handlePictureChange(event){
-    console.log("Event: " + event.currentTarget.id);
-    let value = event.currentTarget.id;
-    let x = this.props.imagesdata[value-1].url_570xN;
+    // These set the state to the event specific image array position (id-1)
     this.setState({
         mainPicture: this.props.imagesdata[event.currentTarget.id-1].url_570xN,
-        ImageCarouselValue: this.props.imagesdata[event.currentTarget.id-1].url_570xN
-      })
+        ImageCarouselValue: event.currentTarget.id-1
+    })
+    // These set the page items to the event specific image array position (id-1)
     this.state.imageCarouselValue = event.currentTarget.id-1;
     this.state.mainPicture = this.props.imagesdata[event.currentTarget.id-1].url_570xN;
-    console.log("mainPicture src: ", this.state.mainPicture);
-    // this.setState({
-    //     mainPicture: this.props.imagesdata[event.currentTarget.id-1].url_570xN
-    //   })
+    // This gets the image by class and the changes the src to the event specific image.
     let place_Image = document.getElementById('mainPictureForCarousel');
     place_Image.src = this.state.mainPicture;
   }
 
-  // plusSlides(n) {
-  // this.state.imageCarouselValue += n;
-  // this.state.mainPicture = this.props.imagesdata[this.state.imageCarouselValue].url_570xN;
-  // let place_Image = document.getElementById('mainPictureForCarousel');
-  // place_Image.src = this.state.mainPicture;
-  // console.log(this.state.mainPicture);
-  // }
-
-  minusSlides(){
-    this.state.imageCarouselValue = this.state.imageCarouselValue - 1;
-    this.state.mainPicture = this.props.imagesdata[this.state.imageCarouselValue].url_570xN;
-    let place_Image = document.getElementById('mainPictureForCarousel');
-    place_Image.src = this.state.mainPicture;
-    console.log(this.state.mainPicture);
-  }
   plusSlides(){
-    // this.state.imageCarouselValue = this.state.imageCarouselValue + 1;
+    // This increases the imageCarouselValue.
     this.state.imageCarouselValue++;
-    let x = this.state.imageCarouselValue - 1;
+    // The code works without the following setState but the this.state.imageCarouselValue appears unchanged.
     this.setState({
-        ImageCarouselValue: x + 1
-      })
-    if(x > this.props.imagesdata){
-      this.state.imageCarouselValue = 0;
-    }
+        ImageCarouselValue: this.state.imageCarouselValue
+    })
     if(this.props.imagesdata[this.state.imageCarouselValue] === undefined){
       this.setState({
           ImageCarouselValue: 0
-        })
-        this.state.imageCarouselValue = 0;
+      })
+      this.state.imageCarouselValue = 0;
     }
-    console.log(this.state.imageCarouselValue);
-
+    // This sets and places the appropriate image from the array.
     this.state.mainPicture = this.props.imagesdata[this.state.imageCarouselValue].url_570xN;
-
     let place_Image = document.getElementById('mainPictureForCarousel');
     place_Image.src = this.state.mainPicture;
-    console.log(this.state.mainPicture);
+  }
+
+  // This function is not working yet.
+  minusSlides(){
+    // This decreases the imageCarouselValue.
+    this.state.imageCarouselValue--;
+    // The code works without the following setState but the this.state.imageCarouselValue appears unchanged.
+    this.setState({
+        ImageCarouselValue: this.state.imageCarouselValue
+    })
+    if(this.props.imagesdata[this.state.imageCarouselValue] === undefined){
+      this.setState({
+          ImageCarouselValue: this.props.imagesdata.length-1
+      })
+      this.state.imageCarouselValue = this.props.imagesdata.length-1;
+    }
+    // This sets and places the appropriate image from the array.
+    this.state.mainPicture = this.props.imagesdata[this.state.imageCarouselValue].url_570xN;
+    let place_Image = document.getElementById('mainPictureForCarousel');
+    place_Image.src = this.state.mainPicture;
   }
 
   render(){
     let imageItem = false;
-    // let link = {};
     if(this.props.imagesdata[0] !== undefined){
       console.log(this.props.imagesdata);
-      // this.setState({
-      //     mainPicture: this.props.imagesdata[0].url_75x75
-      // });
       this.state.mainPicture = this.props.imagesdata[0].url_570xN;
       this.state.imageArray = this.props.imagesdata;
-
     let count = 0;
     imageItem = this.props.imagesdata.map(image => {
       console.log(image);
-      // this.state.imageArray[{count}] = image.url_570xN;
       count++;
       return (
         <div key = {image.id} className = "itemsBoxes">
@@ -105,29 +87,23 @@ export default class ImageCarousel extends Component {
           </div>
         </div>
         )
-        // console.log(this.state.imageArray);
       })
-      console.log(this.state.imageArray);
       }
     return(
       <div className="imagecarousel">
         <p>Carousel:</p>
-        {/* <img src={link[0]} />
-        <img src={link[1]} /> */}
         <div class="slideshow-container">
-
         <img id="mainPictureForCarousel" src={this.state.mainPicture} />
         {/* <a class="prev" onclick={this.minusSlides}>&#10094;</a>
         <a class="next" onclick={this.plusSlides}>&#10095;</a> */}
         <form className="button">
-              <button onClick={this.plusSlides}  id="countDown" type="button" className="btn btn-success">Change Image: {this.state.imageCarouselValue}</button>
-            </form>
+              <button onClick={this.plusSlides}  id="countDown" type="button" className="btn btn-success">Up Image: {this.state.imageCarouselValue}</button>
+        </form>
+        <form className="button">
+              <button onClick={this.minusSlides}  id="countDown" type="button" className="btn btn-success">Down Image: {this.state.imageCarouselValue}</button>
+        </form>
         </div>
         {imageItem}
-        {/* {this.state.imageItems} */}
-         {/* {this.state.pictures} */}
-        {/* <img src={link[2]} />
-        <img src={link[3]} /> */}
       </div>
     )
   }
