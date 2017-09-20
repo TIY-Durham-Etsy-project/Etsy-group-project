1ab905a55768876d395af6f22fbc949b5d2f1a59
Hannah Hudson <hannah.hch@gmail.com>
2017-09-20 14:05:40 -0400

    favorite and add to buttons

--- src/components/socialbuttons.js
+++ src/components/socialbuttons.js
@@ -4,13 +4,13 @@ export default class SocialButtons extends Component {
   render(){
     return(
       <div className="socialbuttons">
-      <button className = "fav-heart">&hearts; Favorite </button>
-      <button className = "add-to-btn">&#9776; Add to &#9662; </button>
-      <hr/>
+        <div className = "favorite-and-addto-btns">
+          <button className = "fav-heart">&hearts; Favorite </button>
+          <button className = "add-to-btn">&#9776; Add to &#9662; </button>
+        </div>
       <a href="http://fontawesome.io/icon/facebook-official/" className="fa fa-facebook">Share</a>
       <a href="http://fontawesome.io/icon/pinterest/" className="fa fa-pinterest"> Save </a>
       <a href="http://fontawesome.io/icon/twitter/" className="fa fa-twitter"> Tweet </a>
-      <hr/>
       </div>
     )
   }
