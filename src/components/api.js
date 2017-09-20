43f295f118420b84bce22dd17eabeeea14b40655
Sam Steele <samsteele@User-Mac.local>
2017-09-20 19:47:22 -0400

    Added filtering functions.

--- src/components/api.js
+++ src/components/api.js
@@ -8,13 +8,18 @@ export default class API extends Component {
     itemObjects: [],
   }
 
+
   let openApi = "https://openapi.etsy.com/v2";
   let apiKey = "api_key=dza1vj8ckkf1tbkxs30wjahj";
-  let include3 = "&category=clothing";
+  // params/filters -- only work with listings/active
+  let size = "limit=100&offset=0";
+  // these might move to category.js
+  let keywords2 = "&keywords=Jewelry";
+  let keywords3 = "&keywords=Clothing";
 
   componentDidMount() {
     // want to change URL with variables for different filters
-    fetch(`${openApi}/listings/trending?${apiKey}&includes=MainImage`)
+    fetch(`${openApi}/listings/active?${apiKey}&includes=MainImage`)
     .then(r => r.json())
     .then(responseData => {
       let dataArray = responseData.r.results
@@ -25,10 +30,29 @@ export default class API extends Component {
     });
   }
 
+  // run for each category
+  function filterArrayByTaxonomyPath(array, category){
+    let catFilteredArray = array.filter((object) => {
+      return object.taxonomy_path = category;
+    })
+    return catFilteredArray;
+  }
+
   // filter through itemObjects to remove missing values
-  let filteredObjects = itemObjects.filter((filterObject) => {
-    return filterObject.state = "active" && filterObject.category_path && filterObject.MainImage.url_170x135
-  })
+  function filterArrayOfMissingData(array){
+    let filteredObjects = itemObjects.filter((filterObject) => {
+      return filterObject.state = "active" && filterObject.taxonomy_path && filterObject.MainImage.url_170x135
+    })
+    console.log(filteredObjects);
+    return filteredObjects;
+  }
+
+  // categories needs ----
+  // need one object from each of 6 categories. Either standard 6 or random
+  // standard = home & living, jewelry, clothing, toys & games, craft supplies, weddings
+
+  // gifts needs ----
+
 
     render() {
       return (<div>MyComponent</div>);
