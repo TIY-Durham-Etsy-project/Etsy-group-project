1ced77a2b838b5ea21bb87e6c39a56afc2c74639
Hannah Hudson <hannah.hch@gmail.com>
2017-09-20 10:55:56 -0400

    add to cart button finished and quantity drop down added

--- src/components/productdescription.js
+++ src/components/productdescription.js
@@ -9,7 +9,27 @@ export default class ProductDescription extends Component {
           <div><h3>$0.00</h3></div>
           <div><button className = "fav-btn">Ask a question</button></div>
         </div>
+        <div className = "options-drop-down-wrapper">
+          <p>Quantity</p>
+            <form>
+              <select className = "quantity-drop-down" placeholder="quantity">
+              <option>1</option>
+              <option>2</option>
+              <option>3</option>
+              <option>4</option>
+              <option>5</option>
+              <option>6</option>
+              <option>7</option>
+              <option>8</option>
+              <option>9</option>
+              <option>10</option>
+            </select>
+        </form>
       </div>
+      <button className = "add-to-cart-btn">
+        Add to cart
+      </button>
+    </div>
     )
   }
 };
