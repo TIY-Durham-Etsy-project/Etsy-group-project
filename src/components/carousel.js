e6b98fbe69c6e48e218c5e2b2dbfed25c49b2566
Katy Dillard <durhamdill@gmail.com>
2017-09-20 20:15:31 -0400

    adds carousel click thru

--- src/components/carousel.js
+++ src/components/carousel.js
@@ -3,17 +3,40 @@ import '../styles/App.css';
 import carouselData from '../data/carousel-data.js';
 
 export default class Carousel extends Component {
+    constructor(props) {
+    super(props);
+
+    this.state = {
+      arrayPosition: 0
+    };
+    this.handlePositionChange = this.handlePositionChange.bind(this);
+  }
+
+  handlePositionChange(event) {
+    event.preventDefault()
+    this.setState({
+      arrayPosition: this.state.arrayPosition + 1
+    })
+    console.log(this.state.arrayPosition);
+  }
+
   render() {
+    let position = this.state.arrayPosition;
     return (
       <div className="carousel-box">
         {/* ----CREATES CAROUSEL SLIDER----- */}
         <div className="carousel-parent">
+          <form onSubmit={this.handlePositionChange}>
+            <button className="carousel-back" type="submit" value="&#60;"></button>
+          </form>
+
+          <div className="carousel-forward">&#62;</div>
           <div className="carousel-textbox">
-            <h1>{carouselData[0].headline}</h1>
-            <h3>{carouselData[0].subhead}</h3>
+            <h1>{carouselData[position].headline}</h1>
+            <h3>{carouselData[position].subhead}</h3>
           </div>
           <div className="carousel-image">
-            <img src={carouselData[0].image} alt=""/>
+            <img height="100%" src={carouselData[position].image} alt=""/>
           </div>
         </div>
 
