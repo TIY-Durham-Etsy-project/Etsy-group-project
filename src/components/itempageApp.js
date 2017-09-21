8d96905f63fcf814b294dbff0abbc4b2a529e6db
Jonathan Canfield <jonathantcanfield@gmail.com>
2017-09-21 13:11:32 -0400

    fix bug with review count on overview listing

--- src/components/itempageApp.js
+++ src/components/itempageApp.js
@@ -97,7 +97,6 @@ class ItemPageApp extends Component {
     fetch(`https://openapi.etsy.com/v2/shops/listing/${this.state.idvariable}?api_key=nn7mkmoan2c7xamo4c3pnax4`).then(results => {
       return results.json();
     }).then(data => {
-      console.log(data.results)
       this.setState({ shopdata: data.results });
       //FETCHES ACTIVE SHOP LISTINGS
       fetch(`https://openapi.etsy.com/v2/shops/${data.results[0].shop_id}/listings/active?limit=5&api_key=nn7mkmoan2c7xamo4c3pnax4`).then(results => {
--- src/components/overview.js
+++ src/components/overview.js
@@ -13,12 +13,11 @@ export default class Overview extends Component {
         <h3 className="overview-title">Overview</h3>
         <ul className = "overview-list">
           {this.props.listingdata.used_manufacturer ? (<li>Manufactured</li>) : (<li>Handmade Item</li>)}
-          <li>Primary Color: </li>
           <li>Category: {this.props.listingdata ? this.props.listingdata.category_path[0] : ""}</li>
           <li>Materials: {this.props.listingdata ? this.props.listingdata.materials.join(", ") : ""}</li>
           <li>When Made: {this.props.listingdata ? this.props.listingdata.when_made.replace(/_/g, " ") : ""}</li>
           <li>Ships {shipsto} From {this.props.listingdata ? this.props.shippinginfodata.origin_country_name : ""} </li>
-          <li>Feedback:<a href = "" className = "etsy-orange-link"> {this.props.shopdata ? this.props.shopdata[2].feedback_info : ""} reviews</a> </li>
+          <li>Feedback:<a href = "" className = "etsy-orange-link"> {this.props.shopdata ? this.props.shopdata[2].feedback_info.count : ""} reviews</a> </li>
           <li>Favorited by: <a href = "" className = "etsy-orange-link">{this.props.listingdata.num_favorers} People</a></li>
           <li>View <a href = "" className = "etsy-orange-link">shop policies</a></li>
         </ul>
