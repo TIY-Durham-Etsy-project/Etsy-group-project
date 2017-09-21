41a204085d2be15ecd4a933853e3651b94a1f2ab
Kevin Harrison <kharrison7@elon.edu>
2017-09-21 15:09:19 -0400

    This lets header items appear upon subheader hovers.

--- src/components/itempageApp.js
+++ src/components/itempageApp.js
@@ -110,7 +110,7 @@ class ItemPageApp extends Component {
           }).then(data => {
             this.setState(prevState => ({
               shoplistingimagesdata: [...prevState.shoplistingimagesdata, data.results[0].url_75x75],
-              shoplistingimagesdatalarge: [...prevState.shoplistingimagesdata, data.results[0].url_170x135]
+              shoplistingimagesdatalarge: [...prevState.shoplistingimagesdatalarge, data.results[0].url_170x135]
             }))
           })
           i++
@@ -140,8 +140,10 @@ class ItemPageApp extends Component {
             <CustomOrder/>
             <DetailsDropdown
             listingdata={this.state.listingdata}/>
-            <Reviews/>
-            <MeetTheOwner/>
+            <Reviews
+            feedbackdata={this.state.feedbackdata}/>
+            <MeetTheOwner
+              shopdata={this.state.shopdata}/>
             <FAQs/>
           </div>
           <div className = "right-content">
@@ -156,7 +158,6 @@ class ItemPageApp extends Component {
             <ShopIcons
               shopdata={this.state.shopdata}
               shoplistingdata={this.state.shoplistingdata}
-              shoplistingimagesdata={this.state.shoplistingimagesdata}
               shoplistingimagesdatalarge={this.state.shoplistingimagesdatalarge}
             />
           </div>
