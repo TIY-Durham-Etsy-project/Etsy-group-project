5bd5a26b98c31149b8c8cd9f79564311fd7014c4
Jonathan Canfield <jonathantcanfield@gmail.com>
2017-09-22 17:55:59 -0400

    add homepage routing on header h1 click

--- src/components/header.js
+++ src/components/header.js
@@ -1,12 +1,19 @@
 import React, { Component } from 'react';
 
 export default class Header extends Component {
+  constructor(props) {
+    super(props);
+    this.sendDataUp = this.sendDataUp.bind(this);
+  }
+  sendDataUp(){
+    this.props.sendDataUpToParent(false);
+  }
   render(){
     return(
       <div className="header">
         <div className="upper-nav">
           <div className="upper-nav-left">
-          <div> <h1 className="etsy-font"> Etsy </h1></div>
+          <div> <h1 onClick={this.sendDataUp} className="etsy-font"> Etsy </h1></div>
           <div className = "search-bar">
           <input className="search-input clearable dropdown-relative-positioned rounded" type="text" placeholder="Search for items or shops"></input>
           <button className="search-button" type="submit" value="Search">Search</button>
