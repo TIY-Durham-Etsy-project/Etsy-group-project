904a54cc63aadc9ec36322dc993b632767d4efff
Jonathan Canfield <jonathantcanfield@gmail.com>
2017-09-22 08:47:08 -0400

    add avg reviews

--- src/components/reviews.js
+++ src/components/reviews.js
@@ -35,6 +35,27 @@ export default class Reviews extends Component {
     }
   }
   render(){
+    let avgRating = false;
+    if (this.props.shopdata){
+      switch (this.props.shopdata.ratings_aggregate) {
+        case 5:
+          avgRating = "fivestarreview.png";
+          break;
+        case 4:
+          avgRating = "fourstarreview.png";
+          break;
+        case 3:
+          avgRating = "threestarreview.png";
+          break;
+        case 2:
+          avgRating = "twostarreview.png";
+          break;
+        case 1:
+          avgRating = "onestarreview.png";
+          break;
+        default:
+      }
+    }
     let reviewsObjects = false;
     if (this.props.feedbackdata){
       reviewsObjects = this.props.feedbackdata.map((x, i)=>{
@@ -50,7 +71,7 @@ export default class Reviews extends Component {
           return (
           <div className="single-user-review" key={x.transaction_id}>
             <p>User id: {this.props.feedbackdata ? x.transaction_id : ""}</p>
-            <p className = "review-stars">Stars: <img alt="" className = "reviews-stars-image-holder" src = {stars ? stars : "no-stars-empty-placeholder-classname"}/></p>
+            <p className = "review-stars">Stars: <img alt="" className = "reviews-stars-image-holder" src = {stars ? stars : ""}/></p>
             <p>{this.props.feedbackdata ? x.message.replace(/&amp;/g, "&").replace(/&gt;/g, ">").replace(/&lt;/g, "<").replace(/&quot;/g, '"') : ""}</p>
           </div>
           )
@@ -58,7 +79,7 @@ export default class Reviews extends Component {
           return (
           <div className="single-user-review" key={x.transaction_id}>
             <p>User id: {this.props.feedbackdata ? x.transaction_id : ""}</p>
-            <p className = "review-stars">Stars: <img alt="" className = "reviews-stars-image-holder" src = {stars ? stars : "no-stars-empty-placeholder-classname"}/></p>
+            <p className = "review-stars">Stars: <img alt="" className = "reviews-stars-image-holder" src = {stars ? stars : ""}/></p>
           </div>
           )
         }
@@ -66,7 +87,7 @@ export default class Reviews extends Component {
     }
     return(
       <div className="reviewsdropdown">
-        <h3 className = "details-header">Reviews  ({this.props.shopdata ? this.props.shopdata[2].feedback_info.count : ""})</h3>
+        <h3 className = "details-header">Reviews  ({this.props.shopdata ? this.props.shopdata[2].feedback_info.count : ""})<img alt="" className = "reviews-stars-image-holder" src = {avgRating ? avgRating : ""}/></h3>
         <div className={this.state.dropdownclassname}>
         {reviewsObjects ? reviewsObjects : ""}
         </div>
